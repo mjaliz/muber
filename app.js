d906e450a41b1b6960a64ea693f138286562edcb
MohammadJavad14 <mrph14@gmail.com>
2023-09-11 16:36:17 +0330

    test create driver

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const routes = require("./routes/routes");
 const app = express();
+
+mongoose.connect("mongodb://localhost/muber");
+app.use(express.json());
 
 routes(app);
 
